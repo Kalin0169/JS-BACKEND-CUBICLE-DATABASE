319f89cb146144a14216008e027b2bef31915fc0
Kalin Hristov <kalin0169@gmail.com>
2023-01-30 14:55:20 +0200

    Save cube to db

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,32 +1,32 @@
-const Cube = require('../models/Cube_old');
+const Cube = require('../models/Cube');
 const db = require('../db.json');
 
 exports.getCreateCube = (req, res) => {
-    res.render('create');
+  res.render('create');
 };
 
-exports.postCreateCube = (req, res) => {
-    const { name, description, imageUrl, difficultyLevel } = req.body;
+exports.postCreateCube = async (req, res) => {
+  const { name, description, imageUrl, difficultyLevel } = req.body;
 
-    let cube = new Cube(name, description, imageUrl, difficultyLevel);
+  let cube = new Cube({ name, description, imageUrl, difficultyLevel });
 
-    cube.save();
+  await cube.save();
 
-    res.redirect('/');
+  res.redirect('/');
 };
 
 exports.getDetails = (req, res) => {
-    let cubeId = Number(req.params.cubeId);
+  let cubeId = Number(req.params.cubeId);
 
-    if (!cubeId) {
-        return res.redirect('/404');
-    }
+  if (!cubeId) {
+    return res.redirect('/404');
+  }
 
-    let cube = db.cubes.find(x => x.id === cubeId);
+  let cube = db.cubes.find((x) => x.id === cubeId);
 
-    if (!cube) {
-        return res.redirect('/404');
-    }
+  if (!cube) {
+    return res.redirect('/404');
+  }
 
-    res.render('details', { cube });
+  res.render('details', { cube });
 };
