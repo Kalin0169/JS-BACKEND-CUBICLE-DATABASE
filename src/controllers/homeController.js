e5ed318dbb436356945266532a6f67d9b30d4bb6
Kalin Hristov <kalin0169@gmail.com>
2023-01-30 15:14:52 +0200

    Show cubes from DB in homepage

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,29 +1,32 @@
-const db = require('../db.json');
+const Cube = require('../models/Cube');
 
-exports.getHomePage = (req, res) => {
-    const { search, from: difficultyFrom, to: difficultyTo } = req.query;
+exports.getHomePage = async (req, res) => {
+  const { search, from: difficultyFrom, to: difficultyTo } = req.query;
 
-    let cubes = db.cubes;
+  let cubes = await Cube.find({}).lean();
 
-    if (search) {
-        cubes = cubes.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
-    }
+  // TODO: Use db filtration instead of in memory filtering
+  if (search) {
+    cubes = cubes.filter((cube) =>
+      cube.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }
 
-    if (difficultyFrom) {
-        cubes = cubes.filter(cube => cube.difficultyLevel >= difficultyFrom);
-    }
+  if (difficultyFrom) {
+    cubes = cubes.filter((cube) => cube.difficultyLevel >= difficultyFrom);
+  }
 
-    if (difficultyTo) {
-        cubes = cubes.filter(cube => cube.difficultyLevel <= difficultyTo);
-    }
+  if (difficultyTo) {
+    cubes = cubes.filter((cube) => cube.difficultyLevel <= difficultyTo);
+  }
 
-    res.render('index', { cubes, search, difficultyFrom, difficultyTo });
+  res.render('index', { cubes, search, difficultyFrom, difficultyTo });
 };
 
 exports.getAboutPage = (req, res) => {
-    res.render('about');
+  res.render('about');
 };
 
 exports.getErrorPage = (req, res) => {
-    res.render('404');
+  res.render('404');
 };
