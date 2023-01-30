99fd9775401fb6498ff9f1841d10e1b1f010b9b1
Kalin Hristov <kalin0169@gmail.com>
2023-01-30 14:43:52 +0200

    Add mongoose
    Add cube model

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,12 @@
 const config = {
-    production: {
-        PORT: 1234,
-    },
-    development: {
-        PORT: 3000,
-    }
+  production: {
+    PORT: 1234,
+    DB_URI: 'mongodb://127.0.0.1:27017/cubicle',
+  },
+  development: {
+    PORT: 3000,
+    DB_URI: 'mongodb://127.0.0.1:27017/cubicle',
+  },
 };
 
 module.exports = config[process.env.node_env || 'development'];
