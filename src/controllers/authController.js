5dfe6d67ef8a999f344656cc93616c8b89ae84ea
Kalin Hristov <kalin0169@gmail.com>
2023-02-04 15:11:39 +0200

    Register user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const authService = require('../services/authService');
 
 router.get('/login', (req, res) => {
   res.render('auth/login');
@@ -8,14 +9,24 @@ router.get('/register', (req, res) => {
   res.render('auth/register');
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
   console.log(req.body);
   const { username, password, repeatPassword } = req.body;
 
   if (password !== repeatPassword) {
     return res.redirect('/404');
   }
-  res.redirect('/');
+
+  const existingUser = await authService.getUserByUsername(username);
+
+  if (existingUser) {
+    return res.redirect('/404');
+  }
+
+  const user = await authService.register(username, password);
+  console.log(user);
+
+  res.redirect('/login');
 });
 
 module.exports = router;
